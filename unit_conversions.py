3cff294ec9a9d3d7cdec449ed5f9ccd9c45290d0
TheDavinator <70241745+TheDavinator@users.noreply.github.com>
2020-12-12 13:08:41 -0800

    added psuedo-code

--- unit_conversions.py
+++ unit_conversions.py
@@ -1,3 +1,32 @@
+""""
+unitMenu
+    Display all available users of coversion
+    for the user to use in this program
+
+grabUnit
+    Opens the unit.txt 
+    reads the file
+    put data into a dictionary
+    find the wanted unit
+    converts it to a float
+    returns the unit
+
+
+doMath
+    uses the grabUnit frunction with "unit.txt" dictionary find the unit to covert from
+    uses grabUnit frunction with "unit.txt" dictionary find the unit to covert from
+    returns the result as a float
+
+main
+    calls the unitMenu function
+    unit_from input from user
+    unit_to input from user
+    amount_yard input from user to provide unit measure for conversion
+    calls doMath function
+    provides final conversion and displays it tot he user
+"""
+
+
 import csv
 
 def unitMenu():
