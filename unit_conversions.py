b55518012eccce6a7672886d63c2a0009ce65c53
TheDavinator <70241745+TheDavinator@users.noreply.github.com>
2020-12-12 12:56:20 -0800

    Added structured-english to project.

--- unit_conversions.py
+++ unit_conversions.py
@@ -1,3 +1,8 @@
+"""
+This program will display nine different units of conversion, ask the user to choose a unit to convert from, a unit to convert to, will take the unit measurement from 
+the user, and provide a converstion for the user base on the options that were choosen. This program utilizes the "units.txt" file a as dictionary with unit 
+conversions store in CSV format. The program opens and reads the selected items to convert from and to and provides the result as a float.
+"""
 import csv
 
 def unitMenu():
