0e759da47aaf1fa758fdefeae4da7a7db093a50c
MarcosValenzuela <marcosvalezuela@mail.fresnostate.edu>
2020-12-13 14:16:41 -0800

    finished project

--- unit_conversions.py
+++ unit_conversions.py
@@ -1,3 +1,65 @@
+"""
+This program will display nine different units of conversion, ask the user to choose a unit to convert from, a unit to convert to, will take the unit measurement from 
+the user, and provide a converstion for the user base on the options that were choosen. This program utilizes the "units.txt" file a as dictionary with unit 
+conversions store in CSV format. The program opens and reads the selected items to convert from and to and provides the result as a float.
+"""
+
+""""
+unitMenu
+    Display all available users of coversion
+    for the user to use in this program
+
+grabUnit
+    Opens the unit.txt 
+    reads the file
+    put data into a dictionary
+    find the wanted unit
+    converts it to a float
+    returns the unit
+
+
+doMath
+    uses the grabUnit frunction with "unit.txt" dictionary find the unit to covert from
+    uses grabUnit frunction with "unit.txt" dictionary find the unit to covert from
+    returns the result as a float
+
+main
+    calls the unitMenu function
+    unit_from input from user
+    unit_to input from user
+    amount_yard input from user to provide unit measure for conversion
+    calls doMath function
+    provides final conversion and displays it tot he user
+"""
+
+""""
+unitMenu
+    Display all available users of coversion
+    for the user to use in this program
+
+grabUnit
+    Opens the unit.txt 
+    reads the file
+    put data into a dictionary
+    find the wanted unit
+    converts it to a float
+    returns the unit
+
+
+doMath
+    uses the grabUnit frunction with "unit.txt" dictionary find the unit to covert from
+    uses grabUnit frunction with "unit.txt" dictionary find the unit to covert from
+    returns the result as a float
+
+main
+    calls the unitMenu function
+    unit_from input from user
+    unit_to input from user
+    amount_yard input from user to provide unit measure for conversion
+    calls doMath function
+    provides final conversion and displays it tot he user
+"""
+
 import csv
 
 def unitMenu():
